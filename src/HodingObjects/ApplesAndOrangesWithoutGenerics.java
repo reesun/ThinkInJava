89736675cdbf7a82d9ed471e7fde5b2ad5c51224
Alex <reesun.huang@gmail.com>
2014-01-02 22:50:52 +0800

    ApplesAndOrangesWithoutGenerics

--- src/HodingObjects/ApplesAndOrangesWithoutGenerics.java
+++ src/HodingObjects/ApplesAndOrangesWithoutGenerics.java
@@ -14,17 +14,16 @@ class Orange {}
 
 public class ApplesAndOrangesWithoutGenerics {
 
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		ArrayList apples = new ArrayList();
+		ArrayList<Apple> apples = new ArrayList<Apple>();
 		for(int i = 0; i < 3; i++){
 			apples.add(new Apple());
 		}
 		
 //		apples.add(new Orange());
 		
-		for(int i = 0; i < apples.size(); i++) {
-			System.out.println(((Apple) apples.get(i)).id());
+		for(Apple apple : apples){
+			System.out.println(apple.id());
 		}
 	}
 
