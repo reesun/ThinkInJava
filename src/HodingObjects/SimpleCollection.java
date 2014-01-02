1a58101f1824883f4295a795cb4afd3a21e1f3fa
Alex <reesun.huang@gmail.com>
2014-01-02 23:18:11 +0800

    SimpleCollection

--- src/HodingObjects/SimpleCollection.java
+++ src/HodingObjects/SimpleCollection.java
@@ -7,9 +7,8 @@ public class SimpleCollection {
 
 	public static void main(String[] args) {
 		Collection<Integer> collection = new ArrayList<Integer>();
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) 
 			collection.add(i);
-		}
 
 		for (Integer i : collection) {
 			System.out.print(i + ", ");
