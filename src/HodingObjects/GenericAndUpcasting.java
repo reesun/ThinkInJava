ab7a93bf55647bae2413e5e6e9eb92acfec5f57e
Alex <reesun.huang@gmail.com>
2014-01-02 22:59:53 +0800

    add GenericAndUpcasting

---
+++ src/HodingObjects/GenericAndUpcasting.java
@@ -0,0 +1,25 @@
+package HodingObjects;
+
+import java.util.ArrayList;
+
+class GrannySmith extends Apple {}
+class Gala extends Apple {}
+class Fuji extends Apple {}
+class Braeburn extends Apple {}
+
+public class GenericAndUpcasting {
+
+	public static void main(String[] args) {
+		ArrayList<Apple> apples = new ArrayList<Apple>();
+		
+		apples.add(new GrannySmith());
+		apples.add(new Gala());
+		apples.add(new Fuji());
+		apples.add(new Braeburn());
+		
+		for (Apple a:apples) {
+			System.out.println(a);
+		}
+	}
+
+}
